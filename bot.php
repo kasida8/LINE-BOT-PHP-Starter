08f8c087486d0be7d2cda9fb09b504e55e0e2249
kasida8 <kasida@outlook.com>
2017-07-23 12:45:27 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -34,15 +34,14 @@ if (!is_null($events['events'])) {
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			curl_setopt($ch, CURLOPT_PROXY, $proxy);
-                        curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-			
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+                        curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 			$result = curl_exec($ch);
 			curl_close($ch);
 
